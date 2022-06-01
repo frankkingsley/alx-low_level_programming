4de3e908ea5d871e488d41d40e7328f9a09c71c7
frank kingsley <frankkingsley911@gmail.com>
2022-06-01 16:10:05 -0700

    my commit message

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *  * create_file - creates a file and fills it with text
- *   * @filename: name of the file to create
- *    * @text_content: text to write in the file
- *     *
- *      * Return: 1 on success, -1 on failure
- *       */
+*  * create_file - creates a file and fills it with text
+*   * @filename: name of the file to create
+*    * @text_content: text to write in the file
+*     *
+*      * Return: 1 on success, -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
 int fd, x, y = 0;
